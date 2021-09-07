351a6b8a68406116884f0ee05561b60be10be76f
ali <ali.ghaieni@gmail.com>
2021-09-07 11:46:19 +0430

    add new subdomain validation

--- subdomain/subdomainFinder.go
+++ subdomain/subdomainFinder.go
@@ -108,7 +108,8 @@ func (r *SubdomainFinder) Enumeration(domain string) ([]string, error) {
 			strings.Contains(k, "cloudflare.net") ||
 			strings.Contains(k, "webproxy") ||
 			strings.HasPrefix(k, "bvr") ||
-			len(k) > (50+len(domain)) {
+			len(k) > (50+len(domain)) ||
+			strings.Count(k, "-") > 3 {
 			continue
 		}
 
